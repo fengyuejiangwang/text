d8d81cce11c70d43d9c8de5d4632d333cd29e707
fengyuejiangwang <2843979718@qq.com>
2022-07-22 16:47:03 +0800

    任何进制转换为10进制

---
+++ home/home1/src/W1286.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class W1286{
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int b= scanner.nextInt();
+       String a=scanner.next();
+        System.out.println("二进制序列("+a+")的值为:"+Integer.parseInt(a,2));
+
+    }
+}
